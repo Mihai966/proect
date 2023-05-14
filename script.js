806c5be86e28da565a4a7d8aacbd49d64eee8e66
Mihai966 <59201201+Mihai966@users.noreply.github.com>
2023-05-14 14:35:10 +0300

    gh

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ function pickDice(dice) {
 
     console.log(image.src)
 
-    if (image.src.match("test1.png")) {
-        image.src = "test2.png";
-    } else if (image.src.match("test2.png")) {
-        image.src = "test3.png";
-    } else if (image.src.match("test3.png")) {
-        image.src = "test1.png";
-        }
+    if (image.src.match("start.png")) {
+        image.src = "logo.png";
+    } else if (image.src.match("logo.png")) {
+        image.src = "slogan.png";
+    } else if (image.src.match("slogan.png")) {
+        image.src = "banner-cover.png";
+    } else if (image.src.match("banner-cover.png")) {
+        image.src = "cartea-de-vizita.png";
+    } else if (image.src.match("cartea-de-vizita.png")) {
+        image.src = "eticheta.png";
+    }
 }
 function pick(dic) {
     var image = document.getElementById(dic);
 
     console.log(image.src)
 
-    if (image.src.match("test3.png")) {
-        image.src = "test2.png";
-    } else if (image.src.match("test2.png")) {
-        image.src = "test1.png";
-    } else if (image.src.match("test1.png")) {
-        image.src = "test3.png";
+    if (image.src.match("eticheta.png")) {
+        image.src = "cartea-de-vizita.png";
+    } else if (image.src.match("cartea-de-vizita.png")) {
+        image.src = "banner-cover.png";
+    } else if (image.src.match("banner-cover.png")) {
+        image.src = "slogan.png";
+    } else if (image.src.match("slogan.png")) {
+        image.src = "logo.png";
+    } else if (image.src.match("logo.png")) {
+        image.src = "start.png";
     }
 }
